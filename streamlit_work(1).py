f9ffe34804d47bca25df325a9e7e5150b5471b60
Phuong123443 <10623037@student.vgu.edu.vn>
2024-05-30 13:54:03 +0700

    Update streamlit_work(1).py

--- streamlit_work(1).py
+++ streamlit_work(1).py
@@ -10,9 +10,10 @@ st.title("Business IT 2|Python")
 st.markdown("IMDB Top 250 Movies")
 st.divider()
 df = pd.read_csv('IMDB Top 250 Movies.csv')
+drama_df = df[df['genre'] == 'Drama']
 st.markdown(
 """
-- **Description**: hello IMDB is a large online movie-database
+- **Description**:IMDB is a large online movie-database
 platform that provides information on films
 and television shows. It includes reviews
 and ratings from users, serving as a
@@ -40,3 +41,29 @@ and genre preferences.
     13. **writers**: Writer of the movie
 """
 )
+tab1= st.tabs(["General relation"])
+with tab1:
+    col1, col2 = st.columns([1,3])
+    with col1:
+        space(lines=10)
+        by_what = st.radio(
+            "Choose a certificate:",
+            ('R', '18+', 'Unrated', 'Not Rated', 'PG-13','Passed'),
+            key = "r1")
+    with col2:
+g = sb.FacetGrid(data=drama_df, col="certificate", col_wrap=3, sharex=False)
+certificates = drama_df['certificate'].unique()
+palette = sb.color_palette("husl", len(certificates))
+
+for i, certificate in enumerate(certificates):
+    subset_df = drama_df[drama_df['certificate'] == certificate]
+    fig1 = px.violin(x="rating", data=subset_df,color=palette[i],ax=g.axes[i],cut=0, inner= None)
+
+g.set_titles(col_template="{col_name}")
+for ax in g.axes.flat:
+    ax.set_xlabel("Rating")
+    ax.set_xticks(np.arange(8.1,9.3,0.2))
+
+plt.subplots_adjust(top=0.9, wspace=0.5, hspace=0.5)
+plt.suptitle("Rating & Certificate among Drama Movies", fontsize=16, fontweight= 'bold')
+st.plotly_chart(fig1a, theme = "streamlit", use_container_width=True)
